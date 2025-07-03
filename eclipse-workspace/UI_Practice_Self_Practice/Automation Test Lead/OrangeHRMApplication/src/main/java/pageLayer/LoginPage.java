aa17b6072f7f8d7a893ab730239256ed9c5072cc
ashok <ashokgambhire5757@gmail.com>
2025-07-03 14:02:30 +0530

    Created project by team lead

---
+++ eclipse-workspace/UI_Practice_Self_Practice/Automation Test Lead/OrangeHRMApplication/src/main/java/pageLayer/LoginPage.java
@@ -0,0 +1,13 @@
+package pageLayer;
+
+public class LoginPage {
+
+	public void loginFunctionality(String userName, String password) 
+	{
+           
+		System.out.println(userName);
+		
+		System.out.println(password);
+	}
+
+}
---
+++ eclipse-workspace/UI_Practice_Self_Practice/Automation Test Lead/OrangeHRMApplication/src/test/java/testLayer/TestLayer.java
@@ -0,0 +1,12 @@
+package testLayer;
+
+import pageLayer.LoginPage;
+
+public class TestLayer {
+
+	public static void main(String[] args) {
+		LoginPage loginPage=new LoginPage();
+		loginPage.loginFunctionality("ashok", "ashok123");
+	}
+
+}
